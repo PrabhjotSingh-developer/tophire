8d3ee22bb4c6250f3262bc0ea888b979c5b74e02
Prabhjot Singh <95993712+PrabhjotSingh-developer@users.noreply.github.com>
2023-07-31 14:57:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,9 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Home from './Pages/Home';
 function App() {
   return (
-       <BrowserRouter>
-            <Routes>
-                 <Route path="/" element={<Home/>}></Route>
-            </Routes>
-         </BrowserRouter>
+      <div>
+               <Home />
+          </div>
   );
 }
 
